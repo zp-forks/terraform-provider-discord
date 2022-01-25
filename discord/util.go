b44535b2051ef079ca5986a8fdcf1e516a4f33d9
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-01-25 19:10:48 +0900

    style: gofmt

--- discord/util.go
+++ discord/util.go
@@ -3,22 +3,22 @@ package discord
 import "hash/crc32"
 
 func Hashcode(s string) int {
-    v := int(crc32.ChecksumIEEE([]byte(s)))
-    if v >= 0 {
-        return v
-    }
-    if -v >= 0 {
-        return -v
-    }
-    // v == MinInt
-    return 0
+	v := int(crc32.ChecksumIEEE([]byte(s)))
+	if v >= 0 {
+		return v
+	}
+	if -v >= 0 {
+		return -v
+	}
+	// v == MinInt
+	return 0
 }
 
 func contains(arr [3]string, str string) bool {
-    for _, a := range arr {
-        if a == str {
-            return true
-        }
-    }
-    return false
+	for _, a := range arr {
+		if a == str {
+			return true
+		}
+	}
+	return false
 }
