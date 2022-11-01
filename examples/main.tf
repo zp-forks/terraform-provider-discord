486b45a78b72c0f225bf911e3d01f2bcfa2f9f30
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-01 21:10:50 +0900

    feat: add example

---
+++ examples/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    discord = {
+      source  = "local/Lucky3028/discord"
+      version = ">=1.0.0"
+    }
+  }
+
+  cloud {
+    organization = "lucky3028"
+
+    workspaces {
+      name = "Kagerou"
+    }
+  }
+}
+
+provider "discord" {
+  token = var.discord_bot_token
+}
+
+variable "discord_bot_token" {
+  type        = string
+  description = "Token of the discord bot."
+  sensitive   = true
+}
+
+resource "discord_server" "server" {
+  name                          = "Kagerou"
+  region                        = "japan"
+  default_message_notifications = 1
+  explicit_content_filter       = 2
+  verification_level            = 4
+  icon_url                      = "https://github.com/Lucky3028/terraform-provider-discord/blob/main/examples/server_icon.jpeg?raw=true"
+}
